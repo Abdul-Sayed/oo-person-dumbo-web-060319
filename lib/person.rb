6a817d5bfd662762fad3f6457e22655bf328d519
Abdul <awsayed@alaska.edu>
2019-06-07 21:01:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,21 @@
 class Person
-  attr_reader :name
-
-  @@bank_account = 25
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
 
   def initialize(name)
     @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
   end
 
-  def bank_account
-    @@bank_account
+  def happiness=(happiness)
+    @happiness = happiness
+    @happiness.clamp(0, 10)
+  end
+
+  def hygiene=(hygiene)
+    @hygiene = hygiene
+    @hygiene.clamp(0, 10)
   end
 end
